0f554a4edae2d15f9d26112652c9a9aacca1de3c
Calandrinon <georgesut@yahoo.com>
2020-10-09 12:13:10 +0300

    Add getters and constructors

--- Lab-2/src/Model/Eggplant.java
+++ Lab-2/src/Model/Eggplant.java
@@ -1,11 +1,18 @@
 package Model;
 
 public class Eggplant {
-    public int get_weight() {
+    private final double weight_in_kg, price;
 
+    Eggplant(double weight_in_kg, double price) {
+        this.weight_in_kg = weight_in_kg;
+        this.price = price;
     }
 
-    public int get_price() {
+    public double get_weight() {
+        return this.weight_in_kg;
+    }
 
+    public double get_price() {
+        return this.price;
     }
 }
--- Lab-2/src/Model/Vegetable.java
+++ Lab-2/src/Model/Vegetable.java
@@ -1,6 +1,6 @@
 package Model;
 
 public interface Vegetable {
-    int get_weight();
-    int get_price();
+    double get_weight();
+    double get_price();
 }
