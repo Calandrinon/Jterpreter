fb1533b7d84f39f98676ce2b5a0e0bc9ac0f687f
Calandrinon <georgesut@yahoo.com>
2020-11-28 13:08:57 +0200

    Add the typechecking method

--- Interpreter/src/Model/Expression/GeneralExpression.java
+++ Interpreter/src/Model/Expression/GeneralExpression.java
@@ -1,11 +1,17 @@
 package Model.Expression;
 import Exceptions.GeneralException;
+import Exceptions.TypeException;
 import Model.ADT.DictionaryInterface;
 import Model.ADT.HeapInterface;
+import Model.Type.Type;
 import Model.Value.Value;
 
 public abstract class GeneralExpression {
     public Value evaluate(DictionaryInterface<String, Value> table, HeapInterface heapInterface) throws GeneralException {
+        return null;
+    }
+
+    public Type typecheck(DictionaryInterface<String, Type> typeEnvironment) {
         return null;
     }
 }
